49a96e03e2ae7239700d679deae38e3484138bd3
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-25 19:03:18 +0400

    ex9.5.1

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    double result = 5;
+    public void getResult() {
+        result = Math.pow(result, 2);
+        result = result + 25;
+        result = result / 3;
+        result = Math.round(result);
+        System.out.println(result);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
-import java.io.File;
+import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) {
-        File files = new File ("/Users/ansarismagilov/Downloads/mosh course");
-        String[] names = files.list();
-        int size = 0;
-        for (var item : names){
-            size += new File("/Users/ansarismagilov/Downloads/mosh course/" + item).length() / 1000000;
-        }
-        System.out.println(size + "MB");
-
+        Calculator calculator = new Calculator();
+        calculator.getResult();
     }
 }
