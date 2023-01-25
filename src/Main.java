f0ecc82edfce1c6029fb7ea8d610e5a691dabd13
Ansar Ismagilov <ansar89kab@bk.ru>
2023-01-25 17:27:58 +0400

    ex9.3.1 sum of all files sizes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
+import java.io.File;
 import java.util.Calendar;
 
 public class Main {
 
     public static void main(String[] args) {
-        Time time = new Time();
-        time.getTime();
+        File files = new File ("/Users/ansarismagilov/Downloads/mosh course");
+        String[] catalog = files.list();
+        double totalBytes = 0.0;
+
+        for (var item : catalog){
+            totalBytes += item.length();
+        }
+
+        System.out.println(totalBytes);
+
     }
 }
